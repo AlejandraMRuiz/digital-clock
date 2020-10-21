018fb416cf760d3ff2d517be7f80f797f8823905
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-21 16:09:15 -0700

    second commit

--- digital-clock.js
+++ digital-clock.js
@@ -1,14 +1,54 @@
 // digital-clock-js
 
-function init() {
-var digiclock = document.getElementById("clock");
-digiclock.innerHTML = new Date();
+//first commit:
+//function init() {
+//var digiclock = document.getElementById("clock");
+//digiclock.innerHTML = new Date();
+//}
+//window.onload = init;
+
+
+//second commit:
+function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
 }
+  
+  function myFunction() {
+    var d = new Date();
+    var digiclock = document.getElementById("clock");
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var s = addZero(d.getSeconds());
+    digiclock.innerHTML = h + ":" + m + ":" + s;
 
-window.onload = init;
 
-//var digiclock = new Date();
-//gets all: day, date and time in HH:MM:SS    
+    //What I've tried that hasn't worked:
 
-//incorporate this somehow in order to make it "tick":
+    //digiclock.innerHTML = setInterval(() => {console.log(d); d++}, 1000);
+    //digiclock.innerHTML = h + ":" + m + ":" + s;
+    //setInterval(() => {new Date(h); new Date(h)++}, 1000);
+    //setInterval(() => {new Date(m); new Date(m)++}, 1000);
+    //setInterval(() => {new Date(s); new Date(s)++}, 1000);
+  }
+
+
+//PLAN:
+
+//Step (1) redo the time display by its parts: HH:MM:SS.
+//***DONE!***
+
+//Step (2) display the time in 24hr-format.
+//***DONE!***
+
+//Step (3) incorporate this somehow in order to make it "tick":
 //setInterval(() => {console.log(counter); counter++}, 1000);
+// y o u   a r e   h e r e
+
+//Step (4) display the day of the week.
+
+//Step (5) display the full date in this format:
+//Wednesday, 21 October 2020
+
