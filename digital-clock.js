a6dc10f22b2f7d20fbc4d66f465c2cc4cab8c78b
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-23 09:24:17 -0700

    adds styling

--- digital-clock.js
+++ digital-clock.js
@@ -1,7 +1,6 @@
 // digital-clock-js
 
-
-//fourth commit: displays day & full date
+// this commit adds styling
 
 function addZero(i) {
   if (i < 10) {
@@ -27,19 +26,8 @@ function myFunction() {
 
 //PLAN:
 
-//Step (1) redo the time display by its parts: HH:MM:SS.
-//***DONE!***
+//Step (1) Get the clock to display without
+//the need of pushing the button.
 
-//Step (2) display the time in 24hr-format.
-//***DONE!***
-
-//Step (3) incorporate this somehow in order to make it "tick":
-//setInterval(() => {console.log(counter); counter++}, 1000);
-//***DONE!***
-
-//Step (4) display the day of the week and the full date in this format:
-//Wednesday, 21 October 2020
-//use the following code for this:
-//var d = new Date();
-//document.getElementById("demo").innerHTML = d.toDateString();
-//***DONE!***
+//Step (2) Get the button to toggle btwn
+//12-24hr formats.
