5ce76f07a7721e66d97bedf03b5b34b2f6563152
Allie Py <alejandramoniqueruizmoreno@Alejandras-MacBook-Air.local>
2020-10-23 18:49:27 -0700

    displays day & time upon opening the site

--- digital-clock.js
+++ digital-clock.js
@@ -1,6 +1,6 @@
 // digital-clock-js
 
-// this commit is a test to separate day & time
+// this commit displays day & time upon opening the site
 
 function addZero(i) {
   if (i < 10) {
@@ -9,29 +9,20 @@ function addZero(i) {
   return i;
 }
 
-
 function myFunction() {
   var d = new Date();
-  // var full_date = document.getElementById("day");
-  // full_date.innerHTML = d.toDateString();
+  var full_date = document.getElementById("day");
+  full_date.innerHTML = d.toDateString();
   var digiclock = document.getElementById("clock");
   var h = addZero(d.getHours());
   var m = addZero(d.getMinutes());
   var s = addZero(d.getSeconds());
   digiclock.innerHTML = h + ":" + m + ":" + s;
-  setInterval(() => {myFunction(); h++}, 1000);
-  setInterval(() => {myFunction(); m++}, 1000);
-  setInterval(() => {myFunction(); s++}, 1000);
 }
 
-//testing below function (which is commented out
-//in "myFunction" above) to attempt to separate day & time
+  setInterval(() => {myFunction(); h++, m++, s++}, 1000);
+  
 
-function day()  {
-  var d = new Date();
-  var full_date = document.getElementById("day");
-  full_date.innerHTML = d.toDateString();
-}
 
 
 
@@ -39,9 +30,11 @@ function day()  {
 
 //Step (0) Separate day & time so day starts 
 //displaying upon opening the site.
+//***DONE!***
 
 //Step (1) Get the clock to display standard
 //time without the need of pushing the button.
+//***DONE!***
 
 //Step (2) Get the button to toggle btwn
 //12-24hr formats.
